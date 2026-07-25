70bf7b9d57c55fab6abddec826a01caae107f711
Gedion Samuel <gedsamhawassa9@gmail.com>
2026-07-25 11:10:28 +0300

    build: configure Prisma and create initial User model

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
-import express, { Request, Response } from "express";
-
+import express, { Request, Response, NextFunction } from "express";
+import healthRoutes from "./modules/health/health.routes";
 const app = express();
+app.use(express.json());
+app.use("/health", healthRoutes);
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("Incoming request");
+    next();
+});
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to EACRMS Backend");
+   res.json({
+    name: "EACRMS Backend",
+    version: "1.0.0",
+    status: "running"
+});
 });
 
 export default app;
