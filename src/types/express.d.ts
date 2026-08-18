b02d1bb82e495b331a0480d65afec7afad6eba4d
Gedion Samuel <gedsamhawassa9@gmail.com>
2026-08-18 23:23:26 +0300

    fix: typecheck

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,13 @@
 import { JwtPayload } from "../utils/jwt";
+import { File as MulterFile } from "multer";
 
 declare global {
   namespace Express {
     interface Request {
       user: JwtPayload;
+    }
+    namespace Multer {
+      type File = MulterFile;
     }
   }
 }
